54adec6a9b1cada100135ee36204c5359426713a
Pankaj Parkar <parkarpankaj3@gmail.com>
2022-10-17 17:06:52 +0800

    Added app.component.ts specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed } from '@angular/core/testing';
+import { flush, TestBed, tick } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -7,25 +8,95 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [FormsModule],
     }).compileComponents();
   });
 
-  it('should create the app', () => {
+  it('should instantiate the component', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    const component = fixture.componentInstance;
+
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'unit-test'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('unit-test');
+  describe('validations', () => {
+    it('should disable button when first parameter field is invalid', () => {
+      // arrange
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      const componentEl = fixture.debugElement.nativeElement;
+      tick();
+
+      // act
+      component.firstNumber = -1;
+      tick();
+
+      // assert
+      expect(componentEl.querySelector('button').disabled).toBeTruthy();
+    });
+
+    it('should disable button when second parameter field is invalid', () => {
+      // arrange
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      const componentEl = fixture.debugElement.nativeElement;
+      fixture.detectChanges();
+
+      // act
+      component.secondNumber = -1;
+      fixture.detectChanges();
+
+      // assert
+      expect(componentEl.querySelector('button').disabled).toBeTruthy();
+    });
+
+    it('should enable button when both fields has valid value', () => {
+      // arrange
+      const fixture = TestBed.createComponent(AppComponent);
+      const component = fixture.componentInstance;
+      const componentEl = fixture.debugElement.nativeElement;
+      fixture.detectChanges();
+
+      // act
+      component.firstNumber = 4;
+      component.secondNumber = 4;
+      fixture.detectChanges();
+
+      // assert
+      expect(componentEl.querySelector('button').disabled).toBeFalsy();
+    });
   });
 
-  it('should render title', () => {
+  it('should not display result if user does not click calculate button', () => {
+    // arrange
     const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const componentEl = fixture.debugElement.nativeElement;
+
+    // act
+    component.firstNumber = 4;
+    component.secondNumber = 4;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('unit-test app is running!');
+
+    // assert
+    expect(componentEl.querySelector('b')).toBeNull();
+  });
+
+  it('should display output after user click calculate button', () => {
+    // arrange
+    const fixture = TestBed.createComponent(AppComponent);
+    const component = fixture.componentInstance;
+    const componentEl = fixture.debugElement.nativeElement;
+
+    // act
+    component.firstNumber = 4;
+    component.secondNumber = 4;
+    componentEl.querySelector('button').click();
+    fixture.detectChanges();
+
+    // assert
+    const result = componentEl.querySelector('b');
+    expect(result).toBeDefined();
+    expect(result.textContent).toContain('Addition is - 8');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { MathService } from './math.service';
 
 @Component({
   selector: 'ut-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Math Operation</h1>
+    <hr>
+    <form class="calculate" ngForm #form="ngForm" (ngSubmit)="calculate()">
+      <input type="number" name="firstNumber" placeholder="First Parameter"
+        [(ngModel)]="firstNumber" min="1" required />
+      &nbsp;
+      <input type="number" name="secondNumber" placeholder="Second Parameter"
+        [(ngModel)]="secondNumber" min="1" required />
+      &nbsp;
+      <button type="submit" [disabled]="form.invalid">
+        Calculate
+      </button>
+    </form>
+    <b *ngIf="total > 0">
+      Addition is - {{ total }}
+    </b>
+  `,
 })
 export class AppComponent {
-  title = 'unit-test';
+  total = 0;
+  firstNumber = 0;
+  secondNumber = 0;
+
+  constructor(
+    private math: MathService,
+  ) { }
+
+  calculate() {
+    this.total = this.math.calculate(this.firstNumber, this.secondNumber);
+  }
 }
